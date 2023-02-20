a646813684c0496428400f38e052f07d4207deaa
Mario Bodemann <mario.bodemann@telekom.de>
2023-02-21 00:33:55 +0100

    feature: add scroll bars

--- app/src/main/kotlin/com/telekom/developer/openapi/explorer/ui/view/OpenAPIView.kt
+++ app/src/main/kotlin/com/telekom/developer/openapi/explorer/ui/view/OpenAPIView.kt
@@ -35,6 +35,7 @@ import com.telekom.developer.openapi.explorer.model.ApiCall
 import com.telekom.developer.openapi.explorer.ui.theme.TMagenta
 import com.telekom.developer.openapi.model.ApiSpecification
 import com.telekom.developer.openapi.model.Operation
+import kotlinx.coroutines.launch
 
 @Composable
 fun OpenAPIView(
@@ -46,6 +47,8 @@ fun OpenAPIView(
     copyText: (text: String) -> Unit,
     loadClicked: () -> Unit,
 ) {
+    val listState = rememberLazyListState()
+
     if (api == null) {
         Button(
             modifier = Modifier.wrapContentSize(),
@@ -54,22 +57,31 @@ fun OpenAPIView(
             Text(stringResource(R.string.select_specification_button))
         }
     } else {
+        var descriptionOffset = 0
+        var apiOffset = 0
+
         LazyColumn(
+            state = listState,
             modifier = Modifier.padding(8.dp),
         ) {
             headerItem(api.info.title)
+            descriptionOffset++
 
             if (api.info.description != null) {
                 item {
                     Text(
                         text = api.info.description.orEmpty()
                     )
+                    descriptionOffset++
                 }
                 itemSpacer()
+                descriptionOffset++
             }
 
             headerItem(titleId = R.string.main_section_apis_title)
+            apiOffset++
             pathItems(api.paths, apiClicked, toggled, toggleClicked)
+            apiOffset += api.paths.size
 
             if (apiCalls.isNotEmpty()) {
                 itemSpacer()
@@ -83,9 +95,57 @@ fun OpenAPIView(
                 )
             }
         }
+
+        CreateScrollers(listState, descriptionOffset, apiOffset, api, apiCalls)
+    }
+}
+
+@Composable
+private fun CreateScrollers(
+    listState: LazyListState,
+    descriptionOffset: Int,
+    apiOffset: Int,
+    api: ApiSpecification,
+    apiCalls: List<ApiCall>
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    fun scrollTo(index: Int) {
+        coroutineScope.launch {
+            when (index) {
+                0 -> listState.animateScrollToItem(0)
+                1 -> listState.animateScrollToItem(descriptionOffset)
+                else -> listState.animateScrollToItem(descriptionOffset + apiOffset)
+            }
+        }
     }
 
-}private fun LazyListScope.headerItem(title: String? = null, @StringRes titleId:Int? = null) {
+    val toDescription = if (api.info.description.isNullOrEmpty()) {
+        null
+    } else {
+        { scrollTo(0) }
+    }
+
+    val toAPIs = if (api.paths.isNotEmpty()) {
+        { scrollTo(1) }
+    } else {
+        null
+    }
+
+    val toCalls = if (apiCalls.isNotEmpty()) {
+        { scrollTo(2) }
+    } else {
+        null
+    }
+
+    Scrollers(
+        toDescription = toDescription,
+        toAPIs = toAPIs,
+        toCalls = toCalls,
+    )
+}
+
+private fun LazyListScope.headerItem(title: String? = null, @StringRes titleId: Int? = null) {
     item {
         Text(
             fontSize = 34.sp,
@@ -117,3 +177,31 @@ private fun LazyListScope.itemSpacer() {
         }
     }
 }
+
+@Composable
+private fun Scrollers(
+    toDescription: (() -> Unit)?,
+    toAPIs: (() -> Unit)?,
+    toCalls: (() -> Unit)?,
+) {
+    Column(modifier = Modifier.wrapContentSize(align = Alignment.BottomEnd)) {
+        if (toDescription != null) {
+            IconButton(onClick = toDescription) {
+                Icon(Icons.Filled.Search, null)
+            }
+        }
+
+        if (toAPIs != null) {
+            IconButton(onClick = toAPIs) {
+                Icon(Icons.Filled.List, null)
+            }
+        }
+
+        if (toCalls != null) {
+            IconButton(onClick = toCalls) {
+                Icon(Icons.Filled.Done, null)
+            }
+        }
+    }
+}
+
